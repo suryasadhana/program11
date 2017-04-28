f45d06baa74fb540493c446cc113842e67ece50b
suryasadhana <suryasadhana74@gmail.com>
2017-04-28 09:01:08 +0530

    Create prgm11.c

---
+++ prgm11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio..h>
+void main()
+{
+clrscr();
+int p,n,i=1;
+long int sum=1;
+printf("enter the value of n:");
+scanf("%d",&n);
+printf("enter the value of p:");
+scanf("%d",&p);
+while(i<=p)
+{
+sum=sum*n;
+i++;
+}
+printf("%d %d %d",n,p,sum;);
+getch();
+}
+
